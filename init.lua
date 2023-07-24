5491932a6912b15e7850fd9535dcaca1d9f8d442
Your Name <you@example.com>
2023-07-24 12:48:01 +0300

    fix

--- init.lua
+++ init.lua
@@ -139,6 +139,22 @@ vim.keymap.set("n", "<C-E>", function ()
      end
 end)
 
+CloseAllFloatingWindows = function()                                                      
+    local closed_windows = {}
+    for _, win in ipairs(vim.api.nvim_list_wins()) do
+      local config = vim.api.nvim_win_get_config(win)
+      if config.relative ~= "" then  -- is_floating_window?                                    
+        vim.api.nvim_win_close(win, false)  -- do not force
+        table.insert(closed_windows, win)
+      end
+    end
+    print(string.format('Closed %d windows: %s', #closed_windows, vim.inspect(closed_windows)))
+  end                            
+
+vim.keymap.set("n", "<Esc>", CloseAllFloatingWindows)
+
+vim.keymap.set("n", "<C-X>", function () vim.cmd(":Lazy") end)
+
 -- Keymaps for better default experience
 -- See `:help vim.keymap.set()`
 vim.keymap.set({ 'n', 'v' }, '<Space>', '<Nop>', { silent = true })
